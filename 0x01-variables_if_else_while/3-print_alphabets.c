996221e4a4e321d8957cf90350b1d8a411543d0a
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-03 13:32:01 -0700

    Task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
-
 /**
  * File: 3-print_alphabets.c
  * Auth: Adeboye Victor Oyekanmi
  *
+ */
+
+#include <stdio.h>
+
+/*
  * main - Prints the alphabet in lowercase, and then in uppercase.
  *
  * Return: Always 0
