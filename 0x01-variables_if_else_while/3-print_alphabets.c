efc4d821d0214dce9ce4ab5f709f4ba10f76baf9
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-03 14:01:44 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,11 +15,11 @@ int main(void)
 {
 	char b;
 
-	for (char b = 'a'; b <= 'z'; b++)
+	for (b = 'a'; b <= 'z'; b++)
 	{
 		putchar(b);
 	}
-	for (char b = 'A'; b <= 'Z'; b++)
+	for (b = 'A'; b <= 'Z'; b++)
 	{
 		putchar(b);
 	}
