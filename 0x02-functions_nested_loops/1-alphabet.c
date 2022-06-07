ab51661c8d4656d814e374e18f3a31a215008398
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-07 16:23:06 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**Author - Adeboye Victor Oyekanmi*/
+
+/**
+ * main - Main Entry
+ *
+ * Description: a function that prints the alphabet, in lowercase
+ *
+ * Return: Always return 0
+ */
+void print_alphabet(void);
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		_putchar(lower);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 Task0 -  a program that prints _putchar, followed by a new line.
+Task1 - a function that prints the alphabet, in lowercase
