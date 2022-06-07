d86b8c316d3665a52a5ab429e043680d01d0c7a4
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-07 15:39:11 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <main.h>
+ 
+/**Author - Adeboye Victor*/
+
+/**
+ * main - check the code
+ *
+ * Description: prints _putchar followed by a new line
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
