28b29406ff78216f0972b4dd4e4715238179f249
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-09 10:10:42 -0700

    Task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**Author: Adeboye Victor Oyekanmi*/
+
+/**
+ * add - Adds two integers and returns
+ * the result.
+ *
+ * @a: number one.
+ * @b: number two.
+ *
+ * Return: Computes number one and number two.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
