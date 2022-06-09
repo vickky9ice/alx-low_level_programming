13881e14c2fa51dc7caf60066604a9ec0aecc04f
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-09 06:20:13 -0700

    Task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,14 @@
 /**
  * main - Main Entry
  *
- * Description: a function that prints the alphabet, in lowercase
+ * print_alphabet: a function that prints lowercase alphabets
  *
  * Return: Always return 0
  */
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
