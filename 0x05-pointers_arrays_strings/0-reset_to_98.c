2ab04b1d6f7c3366961ae409b9b8b046d65c29cf
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-13 11:55:20 -0700

    Resolving error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,13 +4,11 @@
 /**
  * 0-reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to 98
+ * @n: updates value
  */
 
 void reset_to_98(int *n)
 {
-	int *n = 98;
+	*n = 98;
 
-	_putchar("The value of pointer n is: %d\n", n );
-
-	return (98);
 }
