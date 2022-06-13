b0825b985aabc4a206166ffcf066999a730ae54b
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-13 12:19:36 -0700

    Task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**Author: Adeboye Victor Oyekanmi*/
+
+/**
+ * swap_int - swaps the value of two integers
+ * @ab: pointer to swap
+ */
+void swap_int(int *a, int *b);
+{
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
