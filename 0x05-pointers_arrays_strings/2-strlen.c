65481d051002a943fa2623abcd41eea31387808f
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-13 12:59:32 -0700

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**Author: Adeboye Victor Oyekanmi*/
+
+/**
+ * _strlen -  returns the length of a string.
+ * @s: Pointer pointing to a string
+ */
+int _strlen(char *s)
+{
+	strlen(s);
+}
