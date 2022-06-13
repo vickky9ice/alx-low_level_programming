5f9ad60f748b379650e7d634d6af563da733cc4b
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-13 12:00:12 -0700

    Updating description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /** Author: Adeboye Victor Oyekanmi*/
 
 /**
- * 0-reset_to_98 - takes a pointer to an int as parameter
+ * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to 98
  * @n: updates value
  */
