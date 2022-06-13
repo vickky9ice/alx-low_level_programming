f2e5db71688631802945d7b5d821c1da1884fbbf
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-13 12:33:54 -0700

    Correcting errors

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,8 @@
 
 /**
  * swap_int - swaps the value of two integers
- * @ab: pointer to swap
+ * @a: first pointer value to swap with b
+ * @b: second pointer value to swap with a
  */
 void swap_int(int *a, int *b)
 {
