8b176d5a13be2a7c72784070358e574b7c878d4e
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-15 14:52:33 -0700

    Updating code

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stirng.h>
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
