2ad0412c1ad67e1fc7d061a39ac5287c61265721
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-22 07:27:23 -0700

    betty coding issues

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**Author: Adeboye Victor Oyekanmi*/
 
-/**_puts_recursion - prints a string, followed by a new line.
+/**
+ * _puts_recursion - prints a string, followed by a new line.
  * @s: string to print
  * Return: none
  */
