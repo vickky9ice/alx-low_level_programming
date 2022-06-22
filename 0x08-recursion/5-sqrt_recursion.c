5d01579c5a7c88ac5191dd636517f56acba6b766
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-22 08:16:07 -0700

    Task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**Author: Adeboye Victor Oyekanmi*/
+
+/**
+ * _sqrt_recursion - blank
+ * @n: blank
+ * Return: none
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (helper(n, 1));
+}
+
+/**
+ * helper - blank
+ * @n: number
+ * @i: incrementor
+ * Return: -1
+ */
+int helper(int n, int i)
+{
+	if (n == (i * i))
+		return (i);
+	else if (n > (i * i))
+		return (helper(n, i + 1));
+	else
+		return (-1);
+}
