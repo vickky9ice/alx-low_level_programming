7a1daa3e886ff6b20cd331159e24478fa901eef0
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-22 08:32:49 -0700

    Updating code

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,34 +2,45 @@
 /**Author: Adeboye Victor Oyekanmi*/
 
 /**
- * _sqrt_recursion - blank
- * @n: blank
- * Return: none
+ * find_sqrt - Finds the natural square root of an inputted number
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ *
+ * Return: If the number has a natural square root - the square root.
+ * If the number does not have a natural square root - -1.
+ */
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+
+
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(num, root + 1));
+}
+
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ *
+ * Return: If n has a natural square root - the natural square root of n.
+ *  If n does not have a natural square root -1.
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-		return (0);
-	else if (n == 1)
-		return (1);
-	else if (n < 0)
-		return (-1);
-	else
-		return (helper(n, 1));
-}
+	int root = 0;
 
-/**
- * helper - blank
- * @n: number
- * @i: incrementor
- * Return: -1
- */
-int helper(int n, int i)
-{
-	if (n == (i * i))
-		return (i);
-	else if (n > (i * i))
-		return (helper(n, i + 1));
-	else
+	if (n < 0)
 		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
 }
