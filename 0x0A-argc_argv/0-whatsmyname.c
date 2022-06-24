e1683a5593c8f38a5eaada3a398e726620d34a52
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-24 13:12:08 -0700

    Task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**Author: Adeboye Victor Oyekanmi*/
+
+/**
+ * main - my name is
+ * Return: 0
+ * @argc: amount of args
+ * @argv: pointer to pointers array
+ */
+int main(int argc, char *argv[])
+{
+	int c = 0;
+
+	while (c < argc)
+	{
+		if (c == 0)
+		{
+			printf("%s\n", argv[c]);
+			break;
+		}
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c);
+int convert_to_integer(char *s);
+int _atoi(char *s);
+int _strlen_recursion(char *s);
+int only_nums(char *s);
+int is_positive_number(char *number);
+int number_of_coins(int cents);
+
+#endif
