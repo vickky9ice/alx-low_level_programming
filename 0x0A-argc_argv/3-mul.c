d0956d77cf4239f13abe86f5f15057ccf290530e
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-24 13:41:21 -0700

    Task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,19 +9,22 @@
  *
  * Return: 1 if the program does not receive two arguments, otherwise return 0
  */
-int main(int argc, char *argv[])
-{ 
-	int result;
 
-	if (argc < 3 || argc > 3)
+int main(int argc, char *argv[])
+{
+	int num1, num2, prod;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		result = atoi(*(argv + 1)) * atoi(*(argv + 2));
-		printf("%d\n", result);
-	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	printf("%d\n", prod);
+
 	return (0);
 }
