28b3ad7cf15507e5030ca189ac138f1aff1ec1e5
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-24 17:25:04 -0700

    added one more file

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**Author: Adeboye Victor Oyekanmi*/
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
+}
