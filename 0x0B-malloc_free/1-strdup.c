87a4d9246f20cb8c4431f184b17b30e0bbe755a0
Adeboye Victor <adeboyevictor18@gmail.com>
2022-06-27 14:19:48 -0700

    Task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**Author: Adeboye Victor Oyekanmi*/
+
+/**
+ * _strdup - creates and punters of char
+ * @str: the strings
+ *
+ * Return: in punters.
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int x;
+	int y;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (y = 0 ; str[y] != '\0' ; y++);
+	ptr = (char *)malloc(sizeof(char) * (y + 1));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (x = 0 ; str[x] != '\0' ; x++)
+	{
+		ptr[x] = str[x];
+	}
+	return (ptr);
+}
