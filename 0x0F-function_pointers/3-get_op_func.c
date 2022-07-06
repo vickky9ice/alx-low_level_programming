4b3af7a4aabac6310c427f115d64550edd0b59dc
Adeboye Victor <adeboyevictor18@gmail.com>
2022-07-06 13:10:00 -0700

    Task3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,7 +5,7 @@
  * the operation asked by the user.
  * @s: char operator.
  *
- * Return: pointer to the correct function
+ * Return: pointer to the function that corresponds to the operator.
  */
 int (*get_op_func(char *s))(int, int)
 {
