72378e4491208fa13d516d464f9c5f913f080af5
Adeboye Victor <adeboyevictor18@gmail.com>
2022-07-06 13:03:06 -0700

    Task3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,11 @@
 #include "3-calc.h"
 
 /**
- * op_add - adds a and b
- * @a: first number
- * @b: second number
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: sum of a and b
+ * Return: add.
  */
 int op_add(int a, int b)
 {
@@ -13,11 +13,11 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - substracts two numbers
- * @a: first number
- * @b: second number
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: difference
+ * Return: difference.
  */
 int op_sub(int a, int b)
 {
@@ -25,11 +25,11 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - multiplies two numbers
- * @a: first number
- * @b: second number
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: multiplication
+ * Return: multiplication.
  */
 int op_mul(int a, int b)
 {
@@ -37,11 +37,11 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_mod - division of two numbers
- * @a: first number
- * @b: second number
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: division
+ * Return: division.
  */
 int op_div(int a, int b)
 {
