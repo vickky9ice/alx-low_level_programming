23c900490f13172efbc8ea2695397a689c32eac1
Adeboye Victor <adeboyevictor18@gmail.com>
2022-07-25 16:49:11 -0700

    Task4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,10 @@
-#include <main.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
 
 /**
  * print_addr - prints address
