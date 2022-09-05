fe1e9155e1dd26c36622696aa76a9b7ac516f72d
vickky9ice <adeboyevictor18@gmail.com>
2022-09-05 21:57:30 +0100

    Task2

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mul - to add
+ * @x: variable
+ * @y: variable
+ *
+ * Return: the add
+ */
+
+int mul(int x, int y)
+{
+        return (x / y);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * mul - to add
+ * @x: variable
+ * @y: variable
+ *
+ * Return: the add
+ */
+
+int mul(int x, int y)
+{
+        return (x - y);
+}
+
