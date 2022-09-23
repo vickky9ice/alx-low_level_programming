305fa599c4c556531642ab22193215817fbda342
vickky9ice <adeboyevictor18@gmail.com>
2022-09-23 15:42:43 +0100

    Task7

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,7 +35,7 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-/* my funtions proyect 0x1A */
+/* my funtions project 0x1A */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -43,5 +43,16 @@ hash_node_t *add_n_hash(hash_node_t **head, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
+/* 100-sorted_hash_table.c */
+shash_table_t *shash_table_create(unsigned long int size);
+shash_node_t *add_n_shash(shash_node_t **h, const char *key, const char *value);
+void add_i_shash(shash_table_t *ht, shash_node_t *new);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+
 
 #endif
